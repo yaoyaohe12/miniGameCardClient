3d47aa6242f07f199d19587bf4f960791ecd22c6
yaoyaohe12 <287976774@qq.com>
2021-09-22 20:11:24 +0800

    看见别人打的牌

--- game.js
+++ game.js
@@ -20,37 +20,44 @@ const ctx = canvas.getContext('2d')
 var windowHeight = wx.getSystemInfoSync().windowHeight
 var windowWidth = wx.getSystemInfoSync().windowWidth
 
-//头像
-var userImageWidth = 80
-var userPadding = 10
 
 //准备信息
-var readybtn = new myBtn()
+var readybtn = new MyBtn()
 readybtn.words = "  准备"
-readybtn.x = 8*userImageWidth;
-readybtn.y = userImageWidth/2;
-var readyInfos = []
+readybtn.x = windowWidth - readybtn.width;
+readybtn.y = windowHeight - readybtn.height;
+//var readyInfos = []
 
 //card
 var cards = []
-var cardHeight = 100
-var cardWidth = 80
+var allCards = []
+var cardHeight = 60
+var cardWidth = 40
 var cardWordHeight = 20
 var cardGap = 30
 var selectUp = 20
 var cardLineWidth = 3
 
-//showCard
-var showCards = []
+// //showCard
+// var playedCards = []
+
+//
+var otherCards = []
 
 //出牌按钮
-var showBtn = new myBtn()
-showBtn.x = cardWidth
-showBtn.y = windowHeight - cardHeight - 50
+var showBtn = new MyBtn()
+showBtn.x = windowWidth - showBtn.width
+showBtn.y = windowHeight - showBtn.height*3
 showBtn.words = "  出牌"
 
+//跳过按钮
+var skipBtn = new MyBtn()
+skipBtn.x = windowWidth - showBtn.width
+skipBtn.y = windowHeight - showBtn.height*5
+skipBtn.words = "  跳过"
+
 //进入房间按钮
-var EnterBtn = new myBtn()
+var EnterBtn = new MyBtn()
 EnterBtn.x = windowWidth/2
 EnterBtn.y = windowHeight/2
 EnterBtn.words = "进入房间"
@@ -62,11 +69,20 @@ udp.bind()
 //user
 var myNmae = new myWords()
 var myId = -1;
+var mylocalId = 2
 myNmae.x = windowWidth/2
 myNmae.y = 100
 myNmae.words = "游客"
 myNmae.fontSize = "20pt Calibri"
-var userNames = ["游客1","游客2","游客3","游客4","游客5"]
+var userMaxCount = 5 //目前支持5个玩
+//var userNames = ["游客1","游客2","游客3","游客4","游客5"]
+//var userBtn = []
+var userArr = []
+
+//头像
+var userImageWidth = 60
+var userImageHeight = userImageWidth
+var userPadding = 40
 
 //scene
 var sceneEnum = {
@@ -82,10 +98,11 @@ var msgTpye = {
   log_in : 1,
   user_info : 2,
   ready_info : 3,
-  cards_info : 4
+  cards_info : 4,
+  show_cards_info : 5, //别人打出来的牌
 }
 
-function myBtn()
+function MyBtn()
 {
   this.x = 0;
   this.y = 0;
@@ -95,73 +112,79 @@ function myBtn()
   this.bgColor = "#2a5caa" //blue
   this.wordsColor = "#f15b6c" //red
   this.hide = false;
+  this.imageURL
 }
 
-function card()
+function Card(i)
 {
-  this.btn = new myBtn()
-  this.words = "err"
+  this.btn = new MyBtn()
+  // this.words = "err"
+  this.i = i
   this.select = false
   this.btn.width = cardWidth
   this.btn.height = cardHeight
 
-  this.image = new Image()
-  this.image.width = cardWidth
-  this.image.height = cardHeight
+  // this.image = wx.createImage()
+  // this.image.src = "Image/" + i + ".jpg"
 }
 
-function readyInfo(i)
+function User(i)
 {
-  this.words = new myWords()
-  this.words.y = userImageWidth + 20;
-  this.words.x = userImageWidth + (userPadding+userImageWidth)*i;
-  this.words.words = "未准备"
-  this.ready = false;
+  this.btn = new MyBtn()
+  this.btn.words = "游客"
+  this.btn.width = userImageWidth
+  this.btn.height = userImageHeight
+
+  this.readyWords = new myWords()
+  this.ready = false
+
+  this.cards = []
+
+  this.playWords = new myWords()
+  this.playWords.hide = true
+  this.playWords.words = "跳过"
 }
 
-function drawReadyInfo()
-{
-  for(var i = 0; i<readyInfos.length;i++)
-  {
-      if(readyInfos[i].ready)
-      {
-        readyInfos[i].words.words = "已准备"
-      }
-      else{
-        readyInfos[i].words.words = "未准备"
-      }
-      drawWords(readyInfos[i].words)
-  }
-}
-
-function drawName()
-{
-  var textHight = 20
-  var i = 0;
-  var tmp = userImageWidth
-  ctx.font = "8pt Calibri"
-  ctx.fillStyle = "#f15b6c"
-  for(i=0;i<5;i++)
-  {
-    ctx.fillText(userNames[i], tmp+(userImageWidth+userPadding)*i, userImageWidth/2 + textHight)
-  }
-}
+// function readyInfo(i)
+// {
+//   this.words = new myWords()
+//   this.words.y = userImageWidth + 20;
+//   this.words.x = userImageWidth + (userPadding+userImageWidth)*i;
+//   this.words.words = "未准备"
+//   this.ready = false;
+// }
 
 function drawWords(w)
 {
+  if(w.hide) return;
   ctx.font = w.fontSize
   ctx.fillStyle = w.wordsColor
   ctx.fillText(w.words, w.x, w.y)
 }
 
 function drawCards()
-{
-  var i
-  for(i=0;i<cards.length;i++)
-  {
-    drawBtn(cards[i].btn)
+{  
+  // for (var i = 0; i<playedCards.length; i++) {
+  //   var card = playedCards[i]
+  //   drawCustImage(card)
+  // }
+
+  for (var i = 0; i<userMaxCount; i++) {
+    // console.log('card length: ' + userArr[i].cards.length)
+    for (var j = 0; j<userArr[i].cards.length; j++)
+    {
+      var card = userArr[i].cards[j]
+      drawCustImage(card)
+    }
+  }
+
+  //console.log('card length: ' + cards.length)
+  for (var i = 0; i<cards.length; i++) {
+    var card = cards[i]
+    drawCustImage(card)
   }
 }
+
 
 function selectCard(i)
 {
@@ -212,11 +235,12 @@ function lobbyTochHandler(event)
 
 }
 
-function showCard()
+
+function removeCards()
 {
   var i = 0;
   var j = 0;
-
+  var sendCards = []
   for(i=cards.length-1;i>=0;i--)
   {
     if(cards[i].select == true) {
@@ -227,7 +251,40 @@ function showCard()
   {
     cards[i].btn.x = i*cardGap+cardWidth;
   }
+  // console.log('lsp==playedCard: ' + playedCards.length)
+  // playedCards.reverse()
+  // showPlayedCards()
+
   updateCanvas()
+}
+
+function playCards()
+{
+  var i = 0;
+  var j = 0;
+  var sendCards = []
+
+  for(i=0;i<cards.length;i++)
+  {
+    if(cards[i].select == true) {
+      sendCards.push(cards[i].i)
+    }
+  }
+  sendShowCardsInfoMsg(sendCards)
+}
+
+function skipPlay()
+{
+  var sendCards = []
+  sendShowCardsInfoMsg(sendCards)
+}
+
+function showPlayedCards() {
+    for (var i = 0; i < playedCards.length; i++) {
+      var card = playedCards[i]
+      card.btn.x = cardWidth * 2.5 + i*cardGap;
+      card.btn.y = windowHeight - cardHeight * 3;
+    }
 }
 
 function roomTouchHandler(event)
@@ -249,9 +306,17 @@ function roomTouchHandler(event)
   if(isBtnTouched(showBtn, x, y))
   {
     console.log("isShowBtnTouched");
-    showCard()
+    playCards()
     return
   }
+
+  if(isBtnTouched(skipBtn, x, y))
+  {
+    console.log("skipBtn touched");
+    skipPlay()
+    return
+  }
+
   if(isBtnTouched(readybtn, x, y))
   {
     console.log("isBtnTouched")
@@ -277,6 +342,39 @@ export default class WxPatch{
   }
 }
 
+function getLocalId(i)
+{
+
+  
+  var ret = (i + mylocalId - myId + 5)%5 
+  return ret
+}
+
+function initUser()
+{
+  var i
+  for(i=0;i<userMaxCount;i++)
+  {
+    userArr[i] = new User()
+  }
+  var startX =  40
+  var startY =  20
+  userArr[0].btn.x = startX
+  userArr[0].btn.y = startY
+
+  userArr[1].btn.x = userArr[0].btn.x
+  userArr[1].btn.y = userArr[0].btn.y + userImageHeight + userPadding
+
+  userArr[2].btn.x = startX
+  userArr[2].btn.y = windowHeight - userImageHeight - userPadding-cardHeight
+
+  userArr[3].btn.x = windowWidth - userImageWidth
+  userArr[3].btn.y = userArr[0].btn.y+(cardHeight+userPadding)/2 
+
+  userArr[4].btn.x = windowWidth - userImageWidth
+  userArr[4].btn.y = userArr[3].btn.y + userImageHeight + userPadding  
+}
+
 function initGame()
 {
   WxPatch.fixScreen()
@@ -284,30 +382,54 @@ function initGame()
   GetUserInfo()
   canvas.addEventListener('touchstart', touchHandler)
   //
-  InitReadyInfo();
+  loadAllCards()
+  initUser()
 }
 
 
-function initCard(arr , x , y)
+function initCard(arrDest, arr , x , y)
 {
   var i
+  arrDest.splice(0, arrDest.length)
   for(i = 0;i<arr.length;i++)
   {
-    cards[i] = new card(i)
-    cards[i].btn.x = x + i*cardGap;
-    cards[i].btn.y = windowHeight - cardHeight;
-    cards[i].btn.words = arr[i]
+    var cardValue = arr[i]
+    arrDest[i] = new Card(cardValue)
+    arrDest[i].btn.imageURL = "Image/" + cardValue + ".jpg"
+    arrDest[i].btn.x = x + i*cardGap;
+    arrDest[i].btn.y = y;
   }
+
 }
 
 function drawUser()
 {
-  ctx.fillStyle = '#1aad19' // 矩形颜色
-  var i = 0;
-  var tmp = userImageWidth
-  for(i=0;i<5;i++)
+  var i
+  for(i=0;i<userMaxCount;i++)
   {
-    ctx.fillRect(tmp+(userImageWidth+userPadding)*i, 0, userImageWidth, userImageWidth)
+    var u = userArr[i]
+    drawBtn(u.btn)
+    u.readyWords.x = u.btn.x+5
+    u.readyWords.y = u.btn.y+u.btn.height+15
+    if(u.ready)
+    {
+      u.readyWords.words = "已准备"
+    }
+    else{
+      u.readyWords.words = "未准备"
+    }
+
+    u.playWords.y = u.btn.y + u.btn.height/2
+    if(i<=mylocalId)
+    {
+      u.playWords.x = u.btn.x + u.btn.width + 5
+    }
+    else
+    {
+      u.playWords.x = u.btn.x-40
+    }
+    drawWords(u.readyWords)
+    drawWords(u.playWords)
   }
 }
 
@@ -316,8 +438,9 @@ function myWords()
   this.x = 0;
   this.y = 0;
   this.words = "words"
-  this.fontSize = "8pt Calibri"
+  this.fontSize = "12pt Calibri"
   this.wordsColor =  "red" //green
+  this.hide = false;
 }
 
 function drawBtn(btn)
@@ -340,14 +463,17 @@ function updateLobby()
   drawWords(myNmae)
 }
 
-
-function drawCards2()
+function drawCustImage(card)
 {
-  var image = new Image()
-  image.src="Image/1.jpg";
-  image.width = 30
-  image.height = 60
-  ctx.drawImage(image, 200, 200)
+  if (card.btn.imageURL == null) {
+    console.log('lsp===card.btn.image is null: ' + card.i)
+  }
+
+  var image = wx.createImage()
+  image.src = card.btn.imageURL
+  image.onload = function () {
+    ctx.drawImage(image,0,0,card.btn.width, card.btn.height,card.btn.x, card.btn.y,card.btn.width, card.btn.height)
+  }
 }
 
 function updateCanvas()
@@ -360,12 +486,10 @@ function updateCanvas()
   else if(scene == sceneEnum.room)
   {
     drawUser()
-    drawName()
-    drawReadyInfo()
     drawBtn(readybtn)
     drawCards()
     drawBtn(showBtn)
-    drawCards2()
+    drawBtn(skipBtn)
   }
 
 }
@@ -373,6 +497,7 @@ function updateCanvas()
 function sendMsg(bf)
 {
   console.log("sendMsg")
+  console.log(bf);
   udp.send({
     address: serverIp,
     port: serverPort,
@@ -380,14 +505,31 @@ function sendMsg(bf)
   })
 }
 
+function sendShowCardsInfoMsg(arr)
+{
+  console.log("sendShowCardsInfoMsg, myid is "+myId)
+  var nickName = myNmae.words;
+  var bf = new ArrayBuffer(msgHeadSize + 2*arr.length + 4);
+  var v1 = new Uint16Array(bf);
+  var len = arr.length
+  v1[0] = msgTpye.show_cards_info
+  v1[2] = myId
+  v1[3] = len
+  for (var i=0,j=4; i<len; i++) {
+    v1[j++] = arr[i]
+    }
+  console.log(bf);
+  sendMsg(bf)
+}
+
 function sendReadyMsg()
 {
   console.log("sendReadyMsg, myid is"+myId)
-  var nickName = myNmae.words;
-  var bf = new ArrayBuffer(msgHeadSize + 2*nickName.length);
+  var bf = new ArrayBuffer(msgHeadSize + 2);
   var v1 = new Uint16Array(bf);
   v1[0] = msgTpye.ready_info;
   v1[2] = myId;
+  
   sendMsg(bf)
 }
 
@@ -397,9 +539,29 @@ function copy(src, i) {
   return dst;
   }
 
+function getWidthByCardNum(n)
+{
+  var ret =  cardWidth + cardGap*n;
+  return ret
+}
+
+function initShowCardsofUser(i, arr)
+{
+  var y = userArr[i].btn.y
+  var x = 0
+  if(i <=2 )
+  {
+    x = userArr[i].btn.x + userArr[i].btn.width
+  }
+  else{
+    x = userArr[i].btn.x - getWidthByCardNum(arr.length)
+  }
+  initCard(userArr[i].cards, arr, x, y);
+}
+
 function receiveMsg(res)
 {
-  //console.log(res.message)
+  console.log(res.message)
   var heardByteSize = 4;//head占的字节数
   var v1 = new Uint16Array(res.message, 0, 2);
   console.log("reive msg typ is" + v1[0])
@@ -408,7 +570,8 @@ function receiveMsg(res)
     let v2 = new Uint16Array(res.message, 0, 3);
     myId = v2[2]
     console.log("receive log in id = " + myId)
-    userNames[myId] = myNmae.words;
+    //userNames[myId] = myNmae.words;
+    userArr[getLocalId(myId)].btn.words = myNmae.words;
   }
   else if(v1[0] == msgTpye.user_info)
   {
@@ -416,10 +579,10 @@ function receiveMsg(res)
     let receiveId = v2[2]
     let readyStatus = v2[3]
     console.log("receive user info of " + receiveId + "ready status = " + readyStatus)
-    userNames[receiveId] = ab2str(res.message.slice(6))
+    userArr[getLocalId(receiveId)].btn.words = ab2str(res.message.slice(6))
     if(readyStatus)
     {
-      readyInfos[receiveId].ready = true;
+      userArr[getLocalId(receiveId)].ready = true;
     }  
   }
   else if(v1[0] == msgTpye.ready_info)
@@ -427,7 +590,7 @@ function receiveMsg(res)
     let v2 = new Uint16Array(res.message, 0, 3);
     let receiveId = v2[2]
     console.log("receive ready info of " + receiveId)
-    readyInfos[receiveId].ready = true;
+    userArr[getLocalId(receiveId)].ready = true;
   }
   else if(v1[0] == msgTpye.cards_info)
   {
@@ -435,9 +598,26 @@ function receiveMsg(res)
     let receiveId = v2[0]
     let cardSize = v2[1]
     let v3 = new Uint16Array(res.message, heardByteSize + 4, cardSize);
-    initCard(v3, cardWidth, windowHeight - cardHeight);
-    //console.log(receiveId)
-    //console.log(cardSize)
+
+    initCard(cards, v3, cardWidth, windowHeight - cardHeight);
+  }
+  else if(v1[0] == msgTpye.show_cards_info)
+  {
+    let v2 = new Uint16Array(res.message, heardByteSize, 2);
+    let receiveId = v2[0]
+    let cardSize = v2[1]
+    let v3 = new Uint16Array(res.message, heardByteSize + 4, cardSize);
+    let localId = getLocalId(receiveId)
+    console.log("receive id " + receiveId + " local id" + localId);
+    initShowCardsofUser(localId, v3)
+    if(v3.length == 0)
+    {
+      userArr[localId].playWords.hide = false
+    } 
+    else if(receiveId == myId)
+    {
+      removeCards();
+    }
   }
   else{
     console.log("receive invalid")
@@ -533,17 +713,16 @@ function GetUserInfo()
   })
 }
 
-
-function InitReadyInfo()
-{
-  for(var i = 0;i<5;i++)
-  {
-    readyInfos[i] = new readyInfo(i);
-  }
-}
-
 console.log("start")
 initGame()
+
+function loadAllCards() {
+  for (var i = 0; i < 54; i++) {
+    var image = wx.createImage()
+    image.src = "Image/" + (i + 1) + ".jpg"
+    allCards[i] = image
+  }
+}
 
 setTimeout(() => {
   updateCanvas() 
